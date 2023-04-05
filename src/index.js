2218d5166c43ea3697922a524f73bca72bed093e
robine81 <robine.westberg@gmail.com>
2023-04-05 18:05:11 +0200

    Until iteration 4

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector(".price span") 
+  const quantity = product.querySelector(".quantity input")
+  let subTotal = Number(price.innerText) * quantity.value
+  product.querySelector(".subtotal span").innerText = subTotal  //+ is a short hand for converting to string. same as Number()  
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
+
 
   // ITERATION 2
-  //... your code goes here
+  document.querySelectorAll('.product').forEach((oneProduct) => {
+    updateSubtotal(oneProduct);
+  });
 
   // ITERATION 3
-  //... your code goes here
+  let subTotal = 0
+  
+  document.querySelectorAll(".subtotal span").forEach((oneProduct) => {
+    subTotal += Number(oneProduct.innerText)
+  })
+
+  document.querySelector("#total-value span").innerText = subTotal
 }
 
 // ITERATION 4
@@ -34,9 +43,13 @@ function createProduct() {
   //... your code goes here
 }
 
+//event clicking the button starts here
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
+  const removeProductbBtn = document.getElementById('remove');
+  removeProductbBtn.addEventListener('click', removeProduct(Event));  
 
   //... your code goes here
 });
